9071cc8123d6788a85668b35aa894995a9bca3ec
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2024-01-09 00:52:55 +0000

    Create 8-clean_set.js file

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,6 @@
+function cleanSet(set, startString) {
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
+  return filteredValues.map((value) => value.slice(startString.length)).join('-');
+}
+
+export default cleanSet;
