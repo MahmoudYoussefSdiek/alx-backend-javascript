ac045c6be697d968175f537810173280cb87cf65
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2024-01-09 12:27:45 +0000

    Update code in 8-clean_set.js file

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,11 @@
 function cleanSet(set, startString) {
-  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string' || startString === '' || set.size === 0) {
+  if (startString === '') {
     return '';
   }
-
   const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
-  return filteredValues.map((value) => value.slice(startString.length)).join('-');
+  const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
+  const nonEmptyValues = cleanedValues.filter((value) => value !== '');
+  return nonEmptyValues.join('-');
 }
 
 export default cleanSet;
