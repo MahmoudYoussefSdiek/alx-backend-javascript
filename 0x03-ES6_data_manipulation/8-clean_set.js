ca78438ec359a6256181f7449ba56aad932c610d
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2024-01-09 01:04:06 +0000

    Fix code in 8-clean_set.js file

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
   const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
   return filteredValues.map((value) => value.slice(startString.length)).join('-');
 }
